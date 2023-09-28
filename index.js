2c61b6a06873428d7f91f16daf954f95035b589e
Nathan-tugume <144679619+Nathan-tugume@users.noreply.github.com>
2023-09-28 10:18:49 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+// import React from 'react';
+// import LoginPage from './LoginPage';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <LoginPage />
+
+//     </div>
+//   );
+// }
+
+// export default App;
+import React from 'react';
+import ReactDOM from 'react-dom';
+import LoginPage from './LoginPage';
+ReactDOM.render(
+ <React.StrictMode>
+ <LoginPage />
+ </React.StrictMode>,
+ document.getElementById('root')
+);
